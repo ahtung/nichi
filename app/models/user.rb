ae81f03bdd2c7c0c243b877f69feafd7a98fd1b6
Dunya Kirkali <dunyakirkali@gmail.com>
2015-08-08 19:20:32 +0200

    Add event model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
+  # Relations
+  has_many :events, dependent: :destroy, foreign_key: 'owner_id'
+  has_many :contacts, class_name: 'User'
+
   def self.find_for_google_oauth2(access_token, _ = nil)
     data = access_token.info
     user = User.find_by(email: data['email'])
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   xit { should have_many(:contacts).class_name('User') }
-  xit { should have_many(:events) }
+  it { should have_many(:events).dependent(:destroy) }
 end
