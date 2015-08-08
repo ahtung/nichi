f2d773e6ca7111d3d2c37c568f7a8040ad87a793
Dunya Kirkali <dunyakirkali@gmail.com>
2015-08-08 21:02:03 +0200

    Implement create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,17 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to events_path
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def event_params
+    params.require(:event).permit(:name)
   end
 end
--- spec/features/user/create_event_spec.rb
+++ spec/features/user/create_event_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'User', type: :feature do
-  let(:events) { create_list(:event, 1) }
-  let(:user) { create(:user, events: events) }
+  let(:event) { build(:event) }
+  let(:user) { create(:user) }
 
-  xit 'should be able to create an event' do
+  it 'should be able to create an event' do
     login_as user, scope: :user
     visit root_path
     click_on 'New'
-
+    fill_in 'event_name', with: event.name
+    click_on 'Save'
+    expect(page).to have_content(event.name)
   end
 end
