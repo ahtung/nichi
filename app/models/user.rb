936f1c9ed050a024d23eac6fe66b392a50411b8c
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-08-08 16:50:38 +0300

    find for google user method added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.find_for_google_oauth2(access_token, _ = nil)
+    data = access_token.info
+    user = User.find_by(email: data['email'])
+    unless user
+      user = User.create(
+        email: data['email'],
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+    user
+  end
 end
