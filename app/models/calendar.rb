59bd6d194f010fbd8f8b6126c63a34ee5c36693e
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-08-15 00:43:44 +0300

    added new event method to calendar model

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -15,6 +15,27 @@ class Calendar < ActiveRecord::Base
       body: JSON.dump({ 'summary' => 'nichi Calendar' }),
       headers: { 'Content-Type' => 'application/json'}
     )
+    if @calendar.status == 200
+      return google_calendar_id = ActiveSupport::JSON.decode(@calendar.response.body)['id']
+    else
+      return false
+    end
+  end
+
+  def self.new_event
+    @event = {
+      'summary' => 'New Event Title',
+      'description' => 'The description',
+      'start' => { 'dateTime' => Chronic.parse('tomorrow 4 pm') },
+      'end' => { 'dateTime' => Chronic.parse('tomorrow 7pm') },
+    }
+
+    @set_event = @client.execute(
+      api_method: @service.events.insert,
+      parameters: {'calendarId' => calendars.last.google_id, 'sendNotifications' => true},
+      body: JSON.dump(@event),
+      headers: {'Content-Type' => 'application/json'}
+    )
     puts @calendar.inspect
     puts 'helo'
     if @calendar.status == 200
